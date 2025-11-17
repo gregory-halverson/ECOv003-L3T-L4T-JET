221040d207da8e94d47b9be71a81b36edc57d1e9
Gregory Halverson <gregory.halverson@gmail.com>
2025-11-17 15:43:36 -0800

    debugging

--- ECOv003_L3T_L4T_JET/exceptions.py
+++ ECOv003_L3T_L4T_JET/exceptions.py
@@ -2,4 +2,17 @@
 class LPDAACServerUnreachable(Exception):
     pass
 
+# Custom exception for when input files are not accessible
+class InputFilesInaccessible(Exception):
+    pass
+
+# Custom exception for filtering out nighttime data
+class DaytimeFilter(Exception):
+    pass
+
+# Custom exception for when output data is blank
+class BlankOutput(Exception):
+    pass
+
+# Import BlankOutputError from check_distribution for compatibility
 from check_distribution import BlankOutputError
